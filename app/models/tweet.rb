b437526b0deedbecd890836d2caf49d04abf5110
Simon Soto <simon.tomas@hotmail.com>
2020-10-01 23:26:59 -0300

    render tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ApplicationRecord
     belongs_to :user
-    validates :content, presence: true
+    validates :content, presence: true, length: { maximum: 140 }
     has_many :likes, dependent: :destroy
     has_many :linking_users, :through => :likes, :source => :user
 
