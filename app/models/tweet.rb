0c3f15c16f4353a41f12b81f6c5b6e5dc15d16dc
Simon Soto <simon.tomas@hotmail.com>
2020-10-01 22:36:14 -0300

    remove retweet_id

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,6 @@ class Tweet < ApplicationRecord
     validates :content, presence: true, unless: :retweet_id?
     has_many :likes, dependent: :destroy
     has_many :linking_users, :through => :likes, :source => :user
-    has_many :retweets, foreign_key: 'retweet_id'
 
     paginates_per 50
 end
