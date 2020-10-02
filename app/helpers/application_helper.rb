47ec872dd4db66e72b9c467e742f54cc9df36d05
Simon Soto <simon.tomas@hotmail.com>
2020-10-02 20:11:07 -0300

    rounded profile_picture

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
     def avatar_for(user, width = '', height = '')
         @avatar = user.profile_picture
         if @avatar.empty?
-            @avatar_user = image_tag('avatar_default.png', width: width, height: height)
+            @avatar_user = image_tag('avatar_default.png', width: width, height: height, class: 'rounded-circle')
         else
-            @avatar_user = image_tag(user.profile_picture, width: width, height: height)
+            @avatar_user = image_tag(user.profile_picture, width: width, height: height, class: 'rounded-circle')
         end
         return @avatar_user
     end
