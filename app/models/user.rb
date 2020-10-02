3626f85485cd4ab769ed09d64166232207e4d051
Simon Soto <simon.tomas@hotmail.com>
2020-10-02 17:02:29 -0300

    add friends model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :tweets
-  has_many :likes, dependent: :destroy
+  has_many :tweets, dependent: :destroy #Los tweets se eliminan si la cuenta es eliminada
+  has_many :likes, dependent: :destroy #Los likes se eliminan si la cuenta es eliminada
 
   validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 
   URL_REGEXP = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
   validates :profile_picture, format: { with: URL_REGEXP, message: 'You provided invalid URL' }, allow_blank: true
+
 end
