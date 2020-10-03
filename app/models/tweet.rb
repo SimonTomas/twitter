32ab129eb2587960fb043df972938dbc175e386c
Simon Soto <simon.tomas@hotmail.com>
2020-10-02 22:57:32 -0300

    edit tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,8 +3,9 @@ class Tweet < ApplicationRecord
     validates :content, presence: true, length: { maximum: 140 }
     has_many :likes, dependent: :destroy
     has_many :linking_users, :through => :likes, :source => :user
-    belongs_to :source_tweet, optional: true, inverse_of: :retweets, class_name: 'Tweet', foreign_key: 'retweet_id'
-    has_many :retweets, inverse_of: :source_tweet, class_name: 'Tweet', foreign_key: 'retweet_id'
+    # validates :retweet_id, uniqueness: { scope: :user_id }
+    # belongs_to :source_tweet, optional: true, inverse_of: :retweets, class_name: 'Tweet', foreign_key: 'retweet_id'
+    # has_many :retweets, inverse_of: :source_tweet, class_name: 'Tweet', foreign_key: 'retweet_id'
 
     paginates_per 50
 end
