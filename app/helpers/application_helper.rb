07976114fd5a88e48cb29b29972fc7b7fc7cd6ae
Simon Soto <simon.tomas@hotmail.com>
2020-10-08 01:29:30 -0300

    add shadow to profile_picture, add username to posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
     def avatar_for(user, width = '', height = '')
         @avatar = user.profile_picture
         if @avatar.empty?
-            @avatar_user = image_tag('avatar_default.png', width: width, height: height, class: 'rounded-circle')
+            @avatar_user = image_tag('avatar_default.png', width: width, height: height, class: 'rounded-circle img-avatar-users')
         else
-            @avatar_user = image_tag(user.profile_picture, width: width, height: height, class: 'rounded-circle')
+            @avatar_user = image_tag(user.profile_picture, width: width, height: height, class: 'rounded-circle img-avatar-users')
         end
         return @avatar_user
     end
