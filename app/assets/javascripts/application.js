b5a3f2f7ef554932595b3ae99fb7cce6e0d26d0f
Simon Soto <simon.tomas@hotmail.com>
2020-10-09 01:23:54 -0300

    scroll hidden navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,14 @@ $(document).ready(function(){
       {container:'body', trigger: 'hover', placement:"bottom"}
     );   
   });
+
+  var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
